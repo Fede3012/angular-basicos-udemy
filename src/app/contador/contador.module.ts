e2f80a05b3b3ed3cbfe91e59e129a09561fd2f81
Christian Frias <christian_eduardo98@hotmail.com>
2021-03-19 17:43:03 -0500

    primer commit xd

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ContadorComponent } from './contador/contador.component';
+
+@NgModule({
+    imports: [],
+    exports: [ContadorComponent],
+    declarations: [ContadorComponent],
+    providers: [],
+})
+export class ContadorModule { }
